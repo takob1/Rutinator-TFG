95cba6db09e3a486a55ab58a3fccada2f7eb16ad
andy joel valdez alonzo <andyjoelvaldez@gmail.com>
2021-05-24 22:49:10 +0200

    dragandDrop

---
+++ src/app/add-rutina/add-rutina.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { CRUDejercicioService } from '../../compartido/crudejercicio.service';
+import { ToastrService } from 'ngx-toastr';
+import { NavigationExtras, Router } from '@angular/router';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { Ejercicio } from 'src/compartido/ejercicio';
+
+@Component({
+  selector: 'app-add-rutina',
+  templateUrl: './add-rutina.component.html',
+  styleUrls: ['./add-rutina.component.css'],
+})
+export class AddRutinaComponent implements OnInit {
+  ejercicios$ = this.crudApi.ejercicios;
+
+  ej!: Ejercicio[];
+  // ejercicios$ = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  // ejercicioRutina!: Ejercicio[];
+
+  ejercicioRutina = [];
+
+  constructor(
+    private router: Router,
+    public crudApi: CRUDejercicioService,
+    public toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.crudApi.ejercicios.subscribe((exercise: any) => {
+      this.ej = exercise;
+    });
+  }
+
+  onDropped(event: CdkDragDrop<any>) {
+    console.log(event.previousContainer);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+}
